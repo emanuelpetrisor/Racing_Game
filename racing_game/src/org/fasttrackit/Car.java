c66a93520df49b0029ac1710ada0c4a785dbdca2
Emanuel Petrisor <petrisotemanuel10@gmail.com>
2020-12-01 13:32:38 +0200

    inheritance

--- racing_game/src/org/fasttrackit/Car.java
+++ racing_game/src/org/fasttrackit/Car.java
@@ -1,16 +1,9 @@
 package org.fasttrackit;
 
-public class Car {
+public class Car extends Autovehicle {
 
-	String name;
-	double fuelLevel;
-	double mileage;
-	double totalTraveledDistance;
-	double maxSpeed;
-	
 	int doorCount;
-	boolean damaged;
-	String color;
 	
-	Engine engine;
+	
+	
 }
