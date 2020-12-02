9971ef21ee7ad0ef1dcf3e725adc3a86aeeb5830
Emanuel Petrisor <petrisotemanuel10@gmail.com>
2020-12-02 13:18:02 +0200

    added conditional statements

--- racing_game/src/org/fasttrackit/App.java
+++ racing_game/src/org/fasttrackit/App.java
@@ -10,7 +10,7 @@ public class App {
 		carReference.fuelLevel = 60;
 		carReference.mileage = 12.5;
 		carReference.totalTraveledDistance = 0;
-		carReference.damaged = true;
+		carReference.damaged = false;
 		carReference.doorCount = 1;
 		carReference.color = "Red";
 		
@@ -38,6 +38,8 @@ public class App {
 		car2.name = "Renault";
 		car2.fuelLevel = 70;
 		car2.totalTraveledDistance = 100;
+		car2.maxSpeed = 300;
+		car2.mileage = 10;
 		
 		car2.engine = new Engine();
 		car2.engine.capacity = 2000;
@@ -53,8 +55,10 @@ public class App {
 		System.out.println("Door Count: " + car2.doorCount);
 		System.out.println("Color: " + car2.color);
 		
+		System.out.println("************************");
+		
 		double distanceForCar1 = carReference.accelerate(60, 1);
-		double distanceForCar2 = car2.accelerate(100, 1);
+		double distanceForCar2 = car2.accelerate(130, 1);
 		
 		System.out.println("Engine manufacturer before repair: " + carReference.engine.manufacturer);
 		Mechanic mechanic = new Mechanic();
--- racing_game/src/org/fasttrackit/Vehicle.java
+++ racing_game/src/org/fasttrackit/Vehicle.java
@@ -13,6 +13,24 @@ public class Vehicle {
 	
 	public double accelerate(double speed, double durationInHours) {
 		
+		
+		 if (fuelLevel <= 0 || damaged) {
+			 System.out.println("You cannot accelerate.");
+			 return 0;
+		 }
+		
+		
+		
+		 if (speed > maxSpeed) {
+			System.out.println("Max speed exceeded.");
+			return 0;
+		} else if (speed == maxSpeed) {
+			System.out.println("Careful, max speed reached.");
+		} else {
+			System.out.println("Valid speed entered.");
+		}
+		
+		 
 		System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + "h");
 		
 		double distance = speed * durationInHours;
@@ -20,7 +38,14 @@ public class Vehicle {
 		
 		System.out.println("Total traveled distance for vehicle " + name + ": " + totalTraveledDistance );
 		
-		double usedFuel = distance * mileage / 100;
+		double mileageMultiplier = 1;
+		
+		 if (speed > 120) {
+			 mileageMultiplier = speed / 100;
+		 }
+		
+		
+		double usedFuel = distance * mileage * mileageMultiplier / 100;
 		fuelLevel -= usedFuel;
 		System.out.println("Remaining fuel for vehicle " + name + ": " + fuelLevel );
 		
